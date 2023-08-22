31826e6ed41705d390306f3f266c186957317864
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-23 01:44:34 +0530

    problem 01 complete

--- firstProject/hello-world/src/components/HomePage.vue
+++ firstProject/hello-world/src/components/HomePage.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>Hello home from components </h1>
     <h2>{{msg}}</h2>
+    <h1>Simple Counter App</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
   </div>
 </template>
 
@@ -10,6 +14,23 @@ export default {
   name: 'HomePage',
   props: {
     msg:String
+  },
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      if (this.count > 0) {
+        this.count--;
+      }
+    }
   }
+  
 }
 </script>
+
