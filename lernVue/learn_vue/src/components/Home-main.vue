e28b5cbed0338a2ccf0e0c70b3ac0aff1ad3b536
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-24 04:49:37 +0530

    create components

---
+++ lernVue/learn_vue/src/components/Home-main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>home components</h1>
+    <users/>
+    <Products/>
+  </div>
+</template>
+
+<script>
+import users from './users-main.vue'
+import Products from './Products.vue'
+export default {
+  name: "Home-main",
+  components:{
+    users,
+    Products
+  }
+};
+</script>
+
+<style scoped>
+div{
+    background-color: rgb(113, 242, 175);
+    height: 500px;
+    width: 90%;
+    margin: auto;
+
+}
+</style>
---
+++ lernVue/learn_vue/src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1 v-if="show">Conditional Rendring in vue js</h1>
+        <h1 v-else>Conditional Rendring else vue js</h1>
+        <button @click="display">Toggle</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    props:{
+        msg: String,
+        data: String,
+    },
+    data(){
+        return{
+            show:false
+        }
+    },
+    methods:{
+        display(){
+            this.show=!this.show
+        }
+    }
+}
+</script>
+  
