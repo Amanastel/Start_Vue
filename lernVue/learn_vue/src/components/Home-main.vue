acbf17c0c5543cfb9613184d645c6c4e465b647c
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-24 05:19:27 +0530

    some task like home page modifi

--- lernVue/learn_vue/src/components/Home-main.vue
+++ lernVue/learn_vue/src/components/Home-main.vue
@@ -1,32 +1,60 @@
 <template>
   <div>
-    <h1>home components</h1>
-    <users/>
-    <Products/>
+    <p v-html="rowhtml"></p>
+    <div class="home">
+      <div class="flx">
+        <users />
+        <Products />
+      </div>
+      <h1>{{ name }}</h1>
+      <h3>{{ a+b }}</h3>
+      <h3>{{ok?'YES':'NO' }}</h3>
+      <button v-bind:disabled="disbalebtn" v-on:click="updateText" > click me</button>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import users from './users-main.vue'
-import Products from './Products.vue'
+import users from "./users-main.vue";
+import Products from "./Products.vue";
 export default {
   name: "Home-main",
-  components:{
+  components: {
     users,
-    Products
+    Products,
+  },
+  data() {
+    return {
+      name: "Aman",
+      a:10,
+      b:200,
+      ok:true,
+      rowhtml:'<h1>home components</h1>',
+      disbalebtn:false,
+
+    };
+  },
+  methods:{
+    updateText(){
+        this.name="Aman Kumar",
+        this.ok= !this.ok
+        this.disbalebtn = !this.disbalebtn
+    }
   }
 };
 </script>
 
 <style scoped>
-div{
-    background-color: rgb(113, 242, 175);
-    height: 500px;
-    width: 90%;
-    margin: auto;
-
-
-
-    
+.home {
+  background-color: rgb(113, 242, 175);
+  height: 500px;
+  width: 90%;
+  margin: auto;
+  padding: 10px;
+  /* display: flex; */
+}
+.flx{
+    display: flex;
 }
 </style>
