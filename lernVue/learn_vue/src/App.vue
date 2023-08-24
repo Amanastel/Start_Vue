1ba02eea8f1c1870fe8921655c3bbbd92b754b4c
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-24 06:17:01 +0530

    data binding

--- lernVue/learn_vue/src/App.vue
+++ lernVue/learn_vue/src/App.vue
@@ -3,11 +3,11 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <Homes msg="hello world"/> -->
-    <Databind/>
-    <loveme msg="hello world"/>
-    
-    <HomeMain/>
-   
+    <child v-bind:users="users" />
+    <Databind />
+    <loveme msg="hello world" />
+
+    <HomeMain />
   </div>
 </template>
 
@@ -15,20 +15,31 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import Homes from './components/HomePage.vue'
 // import loveme from './components/Love-me.vue'
-import HomeMain from './components/Home-main.vue'
-import Databind from './components/Databind.vue'
-
+import HomeMain from "./components/Home-main.vue";
+import Databind from "./components/Databind.vue";
+import child from "./components/child.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld,
     // Homes,
     // loveme,
     HomeMain,
     Databind,
-  }
-}
+    child,
+  },
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Aman", email: "aman@mail.com" },
+        { id: 2, name: "Rahul", email: "rahul@mail.com" },
+        { id: 3, name: "Sonu", email: "sonu@mail.com" },
+        { id: 4, name: "Kunal", email: "kunal@mail.com" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -41,6 +52,3 @@ export default {
   margin-top: 60px;
 }
 </style>
-
-
-
---
+++ lernVue/learn_vue/src/components/child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- <h1>{{name}}</h1> -->
+        <h1>Child Components</h1>
+        <h3 v-for="user in users" :key="user.name">
+            {{ user.name }} -- {{ user.email }}
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "child-s",
+    props: ['users']
+    
+};
+</script>
