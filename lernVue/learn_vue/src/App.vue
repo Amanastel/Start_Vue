db4279c37b2f9a5219596c2ef72fabacaa923077
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-08-25 03:17:05 +0530

    first prm

--- lernVue/learn_vue/src/App.vue
+++ lernVue/learn_vue/src/App.vue
@@ -2,12 +2,15 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <Homes msg="hello world"/> -->
+    <Homes msg="hello world"/>
     <child v-bind:users="users" />
     <Databind />
     <loveme msg="hello world" />
 
-    <HomeMain />
+    <!-- <HomeMain /> -->
+
+    <h1>{{title}}</h1>
+    <child2 v-on:update-title="changeTitel($event)"/>
   </div>
 </template>
 
@@ -15,9 +18,11 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import Homes from './components/HomePage.vue'
 // import loveme from './components/Love-me.vue'
-import HomeMain from "./components/Home-main.vue";
-import Databind from "./components/Databind.vue";
-import child from "./components/child.vue";
+// import HomeMain from "./components/Home-main.vue";
+// import Databind from "./components/Databind.vue";
+// import child from "./components/child.vue";
+
+import child2 from './components/child2.vue';
 
 export default {
   name: "App",
@@ -25,12 +30,14 @@ export default {
     // HelloWorld,
     // Homes,
     // loveme,
-    HomeMain,
-    Databind,
-    child,
+    // HomeMain,
+    // Databind,
+    // child,
+    child2
   },
   data() {
     return {
+      title: "Parent Title",
       users: [
         { id: 1, name: "Aman", email: "aman@mail.com" },
         { id: 2, name: "Rahul", email: "rahul@mail.com" },
@@ -38,6 +45,11 @@ export default {
         { id: 4, name: "Kunal", email: "kunal@mail.com" },
       ],
     };
+  },
+  methods: {
+    changeTitel: function (data) {
+      this.title = data;
+    },
   },
 };
 </script>
---
+++ lernVue/learn_vue/src/components/child2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button v-on:click="updateTitle">Update Parent Title</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "child-2",
+    methods: {
+        updateTitle() {
+            this.$emit('update-title', 'Updated from child component');
+            
+        }
+    }
+};
+</script>
